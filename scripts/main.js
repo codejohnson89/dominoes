0760084737c5a129109ba585865ee7608ce2fcf1
Micah Johnson <info@micahjjohnson.com>
2017-09-26 18:02:11 -0500

    Initial upload

---
+++ scripts/main.js
@@ -0,0 +1,46 @@
+var teamOneScore = 0;
+var teamTwoScore = 0;
+var updateScore = [5, 10, 15, 20, 25, 30, 35];
+var score;
+var gameCount = document.getElementById("count").innerText;
+
+
+function updateTeamOneScore() {
+    $(".teamOne button").click(function() {
+        score = $(this).val();
+        teamOneScore = teamOneScore + updateScore[score];
+        $("#scoreOne").text(teamOneScore);
+        gameOver();
+    });
+}
+
+function updateTeamTwoScore() {
+    $(".teamTwo button").click(function() {
+        score = $(this).val();
+        teamTwoScore = teamTwoScore + updateScore[score];
+        $("#scoreTwo").text(teamTwoScore);
+        gameOver();
+    });
+}
+
+function gameOver() {
+    if (teamOneScore >= 150 || teamTwoScore >= 150) {
+        console.log("Game Over!");
+        $(".modal-body").append("<p>Final for game " + gameCount + " score, Team One: " + teamOneScore + " Team Two: " + teamTwoScore + "</p>");
+    }
+}
+
+
+
+$("#newGame").click(function() {
+    document.getElementById("scoreOne").innerText = 0;
+    document.getElementById("scoreTwo").innerText = 0;
+    teamOneScore = 0;
+    teamTwoScore = 0;
+    gameCount++;
+    document.getElementById("count").innerText = gameCount;
+});
+
+
+updateTeamOneScore();
+updateTeamTwoScore();
