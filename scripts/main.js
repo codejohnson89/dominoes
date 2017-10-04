e2581b3aaa5cf6a42472e8864f87d08f7affa62b
Micah Johnson <info@micahjjohnson.com>
2017-10-03 19:14:50 -0500

    Test preview version 1
    This includes a 4 player game that will be tested.

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,8 @@
 (function() {
     var teamOneScore = 0;
     var teamTwoScore = 0;
+    var teamThreeScore = 0;
+    var teamFourScore = 0;
     var updateScore = [5, 10, 15, 20, 25, 30, 35];
     var score;
     var gameCount = document.getElementById("count").innerText;
@@ -24,18 +26,40 @@
         });
     }
 
+    function updateTeamThreeScore() {
+        $(".teamThree button").click(function() {
+            score = $(this).val();
+            teamThreeScore = teamThreeScore + updateScore[score];
+            $("#scoreThree").text(teamThreeScore);
+            gameOver();
+        });
+    }
+
+    function updateTeamFourScore() {
+        $(".teamFour button").click(function() {
+            score = $(this).val();
+            teamFourScore = teamFourScore + updateScore[score];
+            $("#scoreFour").text(teamFourScore);
+            gameOver();
+        });
+    }
+
     function gameOver() {
-        if (teamOneScore >= 150 || teamTwoScore >= 150) {
-            console.log("Game Over!");
-            $(".modal-body").append("<p>Final for game " + gameCount + " score, Team One: " + teamOneScore + " Team Two: " + teamTwoScore + "</p>");
+        if (teamOneScore >= 150 || teamTwoScore >= 150 || teamThreeScore >= 150 || teamFourScore >= 150) {
+            window.alert("Game Over!");
+            $(".modal-body").append("<p>Final for game " + gameCount + " score,<br> Player One: " + teamOneScore + " Player Two: " + teamTwoScore + "Player Three: " + teamThreeScore + " Player Four: " + teamFourScore + "</p>");
         }
     }
 
     $("#reset").click(function() {
         document.getElementById("scoreOne").innerText = 0;
         document.getElementById("scoreTwo").innerText = 0;
+        document.getElementById("scoreThree").innerText = 0;
+        document.getElementById("scoreFour").innerText = 0;
         teamOneScore = 0;
         teamTwoScore = 0;
+        teamThreeScore = 0;
+        teamFourScore = 0;
         gameCount = 1;
         document.getElementById("count").innerText = gameCount;
         document.getElementById("results").innerHTML = "";
@@ -46,8 +70,12 @@
     $("#newGame").click(function() {
         document.getElementById("scoreOne").innerText = 0;
         document.getElementById("scoreTwo").innerText = 0;
+        document.getElementById("scoreThree").innerText = 0;
+        document.getElementById("scoreFour").innerText = 0;
         teamOneScore = 0;
         teamTwoScore = 0;
+        teamThreeScore = 0;
+        teamFourScore = 0;
         gameCount++;
         document.getElementById("count").innerText = gameCount;
     });
@@ -55,4 +83,6 @@
 
     updateTeamOneScore();
     updateTeamTwoScore();
+    updateTeamThreeScore();
+    updateTeamFourScore();
 })();
